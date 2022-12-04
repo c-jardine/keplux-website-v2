34eb4b64bf1faf0170a278376700b67be5996c3a
Chris Jardine <chris@keplux.com>
2022-12-04 12:49:43 -0500

    Move base background color to globals.css.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
-import { Box, ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
-      <Box bg="gray.50">
-        <Component {...pageProps} />
-      </Box>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 };
