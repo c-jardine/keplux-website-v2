6b0fbfd46275de39f48658c67dbf92cca89c950c
Chris Jardine <chris@keplux.com>
2022-12-04 12:45:58 -0500

    Wrap app in Box component to change default background color.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Box bg="gray.50">
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
   );
 };
