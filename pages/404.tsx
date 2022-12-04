966c4ab5910228be8d574c02b51e53fcb0df2672
Chris Jardine <chris@keplux.com>
2022-12-03 23:08:44 -0500

    Add barebones custom error page.

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Custom error page.
+ * TODO: Implement the error page.
+ * @returns
+ */
+const ErrorPage = () => {
+  return <div>ErrorPage</div>;
+};
+
+export default ErrorPage;
