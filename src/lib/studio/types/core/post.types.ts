733ebdc271b68dcfb90a48603373338b4e54c74e
Chris Jardine <chris@keplux.com>
2022-12-03 22:28:31 -0500

    Add type definitions for standard Sanity query responses as well as for Author, Series, and Post documents.

---
+++ src/lib/studio/types/core/post.types.ts
@@ -0,0 +1,33 @@
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { SlugProps, SanityResponseBaseProps } from './core.types';
+
+/**
+ * Props returned by Sanity queries on the Author document type.
+ */
+export interface AuthorProps extends SanityResponseBaseProps {
+  _type: 'author';
+  avatar: SanityImageSource;
+  bio: string;
+  name: string;
+  slug: SlugProps;
+}
+
+/**
+ * Props returned by Sanity queries on the Series document type.
+ */
+export interface SeriesProps extends SanityResponseBaseProps {
+  _type: 'series';
+  slug: SlugProps;
+  title: string;
+}
+
+/**
+ * Props returned by Sanity queries on the Post document type.
+ */
+export interface PostProps extends SanityResponseBaseProps {
+  author: AuthorProps;
+  content: unknown;
+  postSeries?: SeriesProps;
+  slug: SlugProps;
+  title: string;
+}
