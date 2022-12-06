5a66e165fc63eaefb8f3bb77d3b36816f1de9265
Chris Jardine <chris@keplux.com>
2022-12-06 11:08:29 -0500

    Add navbar skeleton.

---
+++ src/components/core/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import { Box } from '@chakra-ui/react';
+
+const Navbar = () => {
+  return <Box bg="white" h={16} w="full" shadow="sm"></Box>;
+};
+
+export default Navbar;
--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -2,4 +2,5 @@ export * from './CodeBlock';
 export * from './CodeTabs';
 export * from './CopyCodeButton';
 export * from './CopyCodeInline';
+export * from './Navbar';
 export * from './PortableText';
