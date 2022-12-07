ab95cae09b4cdb4025cc1ac1a1a8c30ad64557c3
Chris Jardine <dev@keplux.com>
2022-12-07 15:59:50 -0500

    Update blog page SEO to handle missing fields.

--- src/components/blog/BlogPageLayout/BlogPageLayout.tsx
+++ src/components/blog/BlogPageLayout/BlogPageLayout.tsx
@@ -1,11 +1,11 @@
 import { Container, Stack } from '@chakra-ui/react';
 import { NextSeo } from 'next-seo';
+import { useRouter } from 'next/router';
 import { PostProps } from '../../../lib/studio/types';
+import { urlForImage } from '../../../lib/studio/urlForImage';
 import _BlogPageAuthorCard from './_BlogPageAuthorCard';
 import _BlogPageHeader from './_BlogPageHeader';
 import _BlogPageMain from './_BlogPageMain';
-import { urlForImage } from '../../../lib/studio/urlForImage';
-import { useRouter } from 'next/router';
 
 /**
  * The main blog page wrapper.
@@ -16,17 +16,19 @@ const BlogPageLayout = (props: PostProps) => {
   return (
     <Container as={Stack} spacing={16} my={16} alignItems="center" maxW="8xl">
       <NextSeo
-        title={seo.seoMetadata.title}
-        description={seo.seoMetadata.description}
+        title={seo.seoMetadata?.title || props.title}
+        description={seo.seoMetadata?.description || props.excerpt || ''}
         canonical={`http://localhost:3000${router.asPath}`}
         openGraph={{
           type: 'article',
           url: `http://localhost:3000${router.asPath}`,
-          title: seo.seoMetadata.title,
-          description: seo.seoMetadata.description,
+          title: seo.seoMetadata?.title || props.title,
+          description: seo.seoMetadata.description || props.excerpt || '',
           images: [
             {
-              url: urlForImage(seo.seoOpenGraph.shareImg).url(),
+              url: seo.seoOpenGraph?.shareImg
+                ? urlForImage(seo.seoOpenGraph.shareImg).url()
+                : urlForImage(props.coverPhoto).url(),
             },
           ],
         }}
