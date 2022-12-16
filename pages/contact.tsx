6d2d2c525e204ad073d22e5a26995277e74267e7
Chris Jardine <chris@keplux.com>
2022-12-15 20:25:25 -0500

    Start updating the contact form.

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,185 +8,294 @@ import {
   Heading,
   HStack,
   Icon,
-  Image as ChakraImage,
   Input,
-  SlideFade,
+  SimpleGrid,
   Stack,
   Text,
   Textarea,
+  useRadio,
+  useRadioGroup,
+  UseRadioProps,
 } from '@chakra-ui/react';
 import { FaFacebook } from '@react-icons/all-files/fa/FaFacebook';
+import { FaLinkedinIn } from '@react-icons/all-files/fa/FaLinkedinIn';
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { SplitSection } from '../src/components/sections';
-import { FaLinkedinIn } from '@react-icons/all-files/fa/FaLinkedinIn';
-import { Parallax } from 'react-scroll-parallax';
+import React from 'react';
+
+const RadioCard = (props: UseRadioProps & { children: string }) => {
+  const { getInputProps, getCheckboxProps } = useRadio(props);
+  const input = getInputProps();
+  const checkbox = getCheckboxProps();
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        py={2}
+        px={4}
+        fontSize="sm"
+        borderWidth="1px"
+        borderRadius="md"
+        textTransform="capitalize"
+        transition="200ms ease-in-out"
+        _checked={{
+          bg: 'brand.600',
+          color: 'white',
+          borderColor: 'brand.600',
+        }}
+        _hover={{
+          bg: 'brand.500',
+          color: 'white',
+          borderColor: 'brand.500',
+        }}
+        _focus={{
+          boxShadow: 'outline',
+        }}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  );
+};
+
+const QuoteFields = (props) => {
+  return (
+    <HStack flexWrap="wrap" w="full" rowGap={2}>
+      <Box borderWidth={1} borderColor="gray.300" rounded="lg" px={2} py={1}>
+        <Text>Starter Package</Text>
+      </Box>
+      <Box borderWidth={1} borderColor="gray.300" rounded="lg" px={2} py={1}>
+        <Text>Professional Package</Text>
+      </Box>
+      <Box borderWidth={1} borderColor="gray.300" rounded="lg" px={2} py={1}>
+        <Text>E-Commerce Package</Text>
+      </Box>
+      <Box borderWidth={1} borderColor="gray.300" rounded="lg" px={2} py={1}>
+        <Text>Ultimate Package</Text>
+      </Box>
+      <Box borderWidth={1} borderColor="gray.300" rounded="lg" px={2} py={1}>
+        <Text>Hosting</Text>
+      </Box>
+      <Box borderWidth={1} borderColor="gray.300" rounded="lg" px={2} py={1}>
+        <Text>Content Management System</Text>
+      </Box>
+      <Box borderWidth={1} borderColor="gray.300" rounded="lg" px={2} py={1}>
+        <Text>E-Commerce</Text>
+      </Box>
+      <Box borderWidth={1} borderColor="gray.300" rounded="lg" px={2} py={1}>
+        <Text>Website update</Text>
+      </Box>
+    </HStack>
+  );
+};
 
 const ContactPage: NextPage = () => {
+  const inquiryTypes = ['quote', 'general'];
+
+  const [formData, setFormData] = React.useState({
+    name: '',
+    email: '',
+    website: '',
+    inquiryType: '',
+    interestedIn: [],
+    message: '',
+  });
+
+  const _handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'inquiryType',
+    value: formData.inquiryType,
+    defaultValue: formData.inquiryType,
+    onChange: _handleChange,
+  });
+
+  const group = getRootProps();
   return (
     <>
-      <Stack id="development" position="relative" w="full" alignItems="center">
-        <Box position="absolute" w="full" h="full" top={0} left={0}>
-          <ChakraImage
-            src="https://images.unsplash.com/photo-1534536281715-e28d76689b4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-            alt="A person typing on a laptop at a desk."
-            objectFit="cover"
-            w="full"
-            h="full"
-          />
-          <Box
-            position="absolute"
-            top={0}
-            bgGradient="linear-gradient(to-br, brand.600, brand.300)"
-            w="full"
-            h="full"
-            opacity={0.95}
-          />
-        </Box>
-        <Container w="full" maxW="4xl" position="relative">
-          <SlideFade in={true}>
-            <Box pt={32} pb={56}>
-              <Parallax speed={-10}>
-                <Heading
-                  as="h1"
-                  fontWeight="black"
-                  color="brand.200"
-                  textAlign="center"
-                >
-                  Ready to <chakra.span color="white">get started</chakra.span>?
-                  Want more information?
-                </Heading>
-                <Stack mt={6} color="whiteAlpha.800">
-                  <Text textAlign="center">
-                    Contact us now to get started on a project or request
-                    additional information. We&apos;ll get back to you within 24
-                    hours.
-                  </Text>
-                </Stack>
-              </Parallax>
-            </Box>
-          </SlideFade>
-        </Container>
-      </Stack>
-      <SplitSection
-        containerStyle={{ maxW: '7xl', w: 'full', mx: 'auto', gap: 16 }}
-      >
-        <SplitSection.LeftContent>
-          <Container position="relative" mt={-16} maxW="2xl" w="full">
-            <Box
-              p={{ base: 4, md: 8 }}
-              rounded="lg"
-              borderBottomWidth={5}
-              borderColor="brand.600"
-              shadow="lg"
-              bg="white"
-            >
-              <Heading as="h2">Send a message</Heading>
-              <Text>Enter your information below</Text>
-              <Stack mt={8} spacing={4}>
-                <FormControl>
-                  <FormLabel
-                    fontSize="sm"
-                    textTransform="uppercase"
-                    fontWeight="semibold"
-                  >
-                    Name
-                  </FormLabel>
-                  <Input />
-                </FormControl>
-                <FormControl>
-                  <FormLabel
-                    fontSize="sm"
-                    textTransform="uppercase"
-                    fontWeight="semibold"
-                  >
-                    Email
-                  </FormLabel>
-                  <Input />
-                </FormControl>
-                <FormControl>
-                  <FormLabel
-                    fontSize="sm"
-                    textTransform="uppercase"
-                    fontWeight="semibold"
-                  >
-                    Phone number
-                  </FormLabel>
-                  <Input />
-                </FormControl>
-                <FormControl>
-                  <FormLabel
-                    fontSize="sm"
-                    textTransform="uppercase"
-                    fontWeight="semibold"
-                  >
-                    Message
-                  </FormLabel>
-                  <Textarea />
-                </FormControl>
-                <Button colorScheme="brand">Send</Button>
-              </Stack>
-            </Box>
-          </Container>
-        </SplitSection.LeftContent>
-        <SplitSection.RightContent>
-          <Container w="full" h="full">
-            <Stack justifyContent="center" h="full" spacing={4} pb={16}>
-              <Heading as="h2">Contact information</Heading>
-              <Stack spacing={8} pl={4}>
-                <Box borderLeftWidth={4} borderColor="brand.600" pl={4} py={2}>
-                  <Text
-                    textTransform="uppercase"
-                    fontWeight="semibold"
-                    color="gray.600"
-                  >
-                    Email
-                  </Text>
-                  <Link href="mailto:contact@keplux.com">
-                    <chakra.span color="gray.500">
-                      contact@keplux.com
-                    </chakra.span>
-                  </Link>
-                </Box>
-                <Stack
-                  borderLeftWidth={4}
-                  borderColor="brand.600"
-                  pl={4}
-                  py={2}
-                >
-                  <Text
-                    textTransform="uppercase"
-                    fontWeight="semibold"
-                    color="gray.600"
-                  >
-                    Social Media
-                  </Text>
-                  <HStack>
-                    <Icon
-                      as={FaFacebook}
-                      color="gray.500"
-                      w={6}
-                      h={6}
-                      _hover={{ color: 'black' }}
-                      transition="200ms ease-in-out"
-                      cursor="pointer"
-                    />
-                    <Icon
-                      as={FaLinkedinIn}
-                      color="gray.500"
-                      w={6}
-                      h={6}
-                      _hover={{ color: 'black' }}
-                      transition="200ms ease-in-out"
-                      cursor="pointer"
-                    />
-                  </HStack>
-                </Stack>
-              </Stack>
+      <Stack alignItems="center" spacing={16} py={28}>
+        <Container maxW="7xl" w="full">
+          <Stack alignItems="center" spacing={12}>
+            <Stack alignItems="center">
+              <Heading
+                as="h1"
+                fontSize={['5xl', , '6xl', '7xl']}
+                fontWeight="black"
+                textAlign="center"
+                color="black"
+              >
+                Send a message to get started or learn more
+              </Heading>
+              <Heading
+                as="h2"
+                maxW="2xl"
+                w="full"
+                fontSize="xl"
+                textAlign="center"
+                fontWeight="medium"
+                color="gray.500"
+              >
+                Need a quote or looking for more information? Get in touch with
+                one of the methods below and we&apos;ll get back to you soon.
+              </Heading>
             </Stack>
-          </Container>
-        </SplitSection.RightContent>
-      </SplitSection>
+          </Stack>
+        </Container>
+
+        <SplitSection
+          containerStyle={{ maxW: '7xl', w: 'full', mx: 'auto', gap: 16 }}
+        >
+          <SplitSection.LeftContent>
+            <Container position="relative" maxW="2xl" w="full">
+              <Box
+                p={{ base: 4, md: 8 }}
+                rounded="lg"
+                borderWidth={1}
+                borderColor="gray.300"
+              >
+                <Stack spacing={4}>
+                  <SimpleGrid columns={{ base: 1, lg: 2 }} gap={4}>
+                    <FormControl>
+                      <FormLabel
+                        marginBottom={1}
+                        fontSize="xs"
+                        fontWeight="normal"
+                        letterSpacing="wide"
+                      >
+                        Name
+                      </FormLabel>
+                      <Input />
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel
+                        marginBottom={1}
+                        fontSize="xs"
+                        fontWeight="normal"
+                        letterSpacing="wide"
+                      >
+                        Email
+                      </FormLabel>
+                      <Input />
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel
+                        marginBottom={1}
+                        fontSize="xs"
+                        fontWeight="normal"
+                        letterSpacing="wide"
+                      >
+                        Website
+                      </FormLabel>
+                      <Input />
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel
+                        marginBottom={1}
+                        fontSize="xs"
+                        fontWeight="normal"
+                        letterSpacing="wide"
+                      >
+                        Inquiry Type
+                      </FormLabel>
+                      <HStack {...group}>
+                        {inquiryTypes.map((value) => {
+                          const radio = getRadioProps({ value });
+                          return (
+                            <RadioCard key={value} {...radio}>
+                              {value}
+                            </RadioCard>
+                          );
+                        })}
+                      </HStack>
+                    </FormControl>
+                  </SimpleGrid>
+                  {inqType === 'quote' && <QuoteFields />}
+                  <FormControl>
+                    <FormLabel
+                      marginBottom={1}
+                      fontSize="xs"
+                      fontWeight="normal"
+                      letterSpacing="wide"
+                    >
+                      Message
+                    </FormLabel>
+                    <Textarea />
+                  </FormControl>
+                  <Button variant="primary">Send</Button>
+                </Stack>
+              </Box>
+            </Container>
+          </SplitSection.LeftContent>
+          <SplitSection.RightContent>
+            <Container w="full" h="full">
+              <Stack justifyContent="center" h="full" spacing={4} pb={16}>
+                <Heading as="h2">Contact information</Heading>
+                <Stack spacing={8} pl={4}>
+                  <Box
+                    borderLeftWidth={4}
+                    borderColor="brand.600"
+                    pl={4}
+                    py={2}
+                  >
+                    <Text
+                      textTransform="uppercase"
+                      fontWeight="semibold"
+                      color="gray.600"
+                    >
+                      Email
+                    </Text>
+                    <Link href="mailto:contact@keplux.com">
+                      <chakra.span color="gray.500">
+                        contact@keplux.com
+                      </chakra.span>
+                    </Link>
+                  </Box>
+                  <Stack
+                    borderLeftWidth={4}
+                    borderColor="brand.600"
+                    pl={4}
+                    py={2}
+                  >
+                    <Text
+                      textTransform="uppercase"
+                      fontWeight="semibold"
+                      color="gray.600"
+                    >
+                      Social Media
+                    </Text>
+                    <HStack>
+                      <Icon
+                        as={FaFacebook}
+                        color="gray.500"
+                        w={6}
+                        h={6}
+                        _hover={{ color: 'black' }}
+                        transition="200ms ease-in-out"
+                        cursor="pointer"
+                      />
+                      <Icon
+                        as={FaLinkedinIn}
+                        color="gray.500"
+                        w={6}
+                        h={6}
+                        _hover={{ color: 'black' }}
+                        transition="200ms ease-in-out"
+                        cursor="pointer"
+                      />
+                    </HStack>
+                  </Stack>
+                </Stack>
+              </Stack>
+            </Container>
+          </SplitSection.RightContent>
+        </SplitSection>
+      </Stack>
     </>
   );
 };
