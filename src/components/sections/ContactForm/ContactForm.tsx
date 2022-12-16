ea874bbba2ee437aeae827c09dc16b5c3d1eb296
Chris Jardine <chris@keplux.com>
2022-12-16 12:13:54 -0500

    Add react-hook-form for contact form and create custom radio group.

--- src/components/sections/ContactForm/ContactForm.tsx
+++ src/components/sections/ContactForm/ContactForm.tsx
@@ -3,78 +3,148 @@ import {
   Button,
   Container,
   FormControl,
+  FormErrorMessage,
   FormLabel,
-  HStack,
   Input,
   SimpleGrid,
+  Spinner,
   Stack,
   Text,
   Textarea,
-  useRadioGroup,
 } from '@chakra-ui/react';
 import axios from 'axios';
 import React from 'react';
-import { RadioCard } from '../../core';
+import { useForm } from 'react-hook-form';
 import { ContactInterestedInOptions } from '../ContactInterestedInOptions';
 import { ContactFormProps } from './ContactForm.types';
+import ContactFormInquiryOptions from './ContactFormInquiryOptions';
 
-const InquiryTypes = ['quote', 'general'];
+const InitialFormState: ContactFormProps = {
+  name: '',
+  email: '',
+  website: '',
+  inquiryType: 'general',
+  interestedIn: [],
+  message: '',
+};
 
 const ContactForm = () => {
-  const [formData, setFormData] = React.useState<ContactFormProps>({
-    name: '',
-    email: '',
-    website: '',
-    inquiryType: 'general',
-    interestedIn: [],
-    message: '',
-  });
+  const [formData, setFormData] =
+    React.useState<ContactFormProps>(InitialFormState);
 
-  const { getRootProps, getRadioProps } = useRadioGroup({
-    name: 'inquiryType',
-    value: formData.inquiryType,
-    defaultValue: formData.inquiryType,
-    onChange: (e: 'quote' | 'general') =>
-      setFormData({ ...formData, inquiryType: e }),
-  });
+  const {
+    handleSubmit,
+    register,
+    watch,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<ContactFormProps>();
 
-  const group = getRootProps();
-
-  const _handleChange = (e: { target: { name: string; value: string } }) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const _handleSubmit = async () => {
-    const res = await axios.post('/api/submitContactForm', { ...formData });
-    const data = await res.data;
-    console.log(data);
+  const onSubmit = async () => {
+    try {
+      await axios.post('/api/submitContactForm', { ...formData });
+      setFormData(InitialFormState);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
     <Container position="relative" maxW="2xl" w="full">
+      {/* <Text>hi {watch('inquiryType')}</Text> */}
       <Box
         p={{ base: 4, md: 8 }}
         rounded="lg"
         borderWidth={1}
         borderColor="gray.300"
       >
-        <Stack spacing={4}>
-          <SimpleGrid columns={{ base: 1, lg: 2 }} gap={4}>
-            <FormControl>
-              <FormLabel
-                marginBottom={1}
-                fontSize="xs"
-                fontWeight="normal"
-                letterSpacing="wide"
-              >
-                Name
-              </FormLabel>
-              <Input
-                name="name"
-                value={formData.name}
-                onChange={_handleChange}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack spacing={4}>
+            <SimpleGrid columns={{ base: 1, lg: 2 }} gap={4}>
+              <FormControl isInvalid={!!errors.name}>
+                <FormLabel
+                  marginBottom={1}
+                  fontSize="xs"
+                  fontWeight="normal"
+                  letterSpacing="wide"
+                >
+                  Name
+                </FormLabel>
+                <Input
+                  id="name"
+                  name="name"
+                  disabled={isSubmitting}
+                  {...register('name', {
+                    required: 'Required',
+                    minLength: { value: 2, message: 'Too short' },
+                  })}
+                />
+                <FormErrorMessage fontSize="xs">
+                  {errors.name && errors.name.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <FormLabel
+                  marginBottom={1}
+                  fontSize="xs"
+                  fontWeight="normal"
+                  letterSpacing="wide"
+                >
+                  Email
+                </FormLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  disabled={isSubmitting}
+                  {...register('email', {
+                    required: 'Required',
+                  })}
+                />
+                <FormErrorMessage fontSize="xs">
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <FormLabel
+                  marginBottom={1}
+                  fontSize="xs"
+                  fontWeight="normal"
+                  letterSpacing="wide"
+                >
+                  Website
+                </FormLabel>
+                <Input
+                  id="website"
+                  name="website"
+                  disabled={isSubmitting}
+                  {...register('website')}
+                />
+                <FormErrorMessage fontSize="xs">
+                  {errors.website && errors.website.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <FormLabel
+                  marginBottom={1}
+                  fontSize="xs"
+                  fontWeight="normal"
+                  letterSpacing="wide"
+                >
+                  Inquiry Type
+                </FormLabel>
+                <ContactFormInquiryOptions
+                  name="inquiryType"
+                  control={control}
+                />
+              </FormControl>
+            </SimpleGrid>
+            {watch('inquiryType') === 'quote' && (
+              <ContactInterestedInOptions
+                formData={formData}
+                setFormData={setFormData}
               />
-            </FormControl>
+            )}
             <FormControl>
               <FormLabel
                 marginBottom={1}
@@ -82,75 +152,26 @@ const ContactForm = () => {
                 fontWeight="normal"
                 letterSpacing="wide"
               >
-                Email
+                Message
               </FormLabel>
-              <Input
-                name="email"
-                value={formData.email}
-                onChange={_handleChange}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel
-                marginBottom={1}
-                fontSize="xs"
-                fontWeight="normal"
-                letterSpacing="wide"
-              >
-                Website
-              </FormLabel>
-              <Input
-                name="website"
-                value={formData.website}
-                onChange={_handleChange}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel
-                marginBottom={1}
-                fontSize="xs"
-                fontWeight="normal"
-                letterSpacing="wide"
-              >
-                Inquiry Type
-              </FormLabel>
-              <HStack {...group}>
-                {InquiryTypes.map((value) => {
-                  const radio = getRadioProps({ value });
-                  return (
-                    <RadioCard key={value} {...radio}>
-                      {value}
-                    </RadioCard>
-                  );
+              <Textarea
+                id="message"
+                name="message"
+                disabled={isSubmitting}
+                {...register('message', {
+                  required: 'Required',
+                  minLength: { value: 32, message: 'Provide more details' },
                 })}
-              </HStack>
+              />
+              <FormErrorMessage fontSize="xs">
+                {errors.message && errors.message.message}
+              </FormErrorMessage>
             </FormControl>
-          </SimpleGrid>
-          {formData.inquiryType === 'quote' && (
-            <ContactInterestedInOptions
-              formData={formData}
-              setFormData={setFormData}
-            />
-          )}
-          <FormControl>
-            <FormLabel
-              marginBottom={1}
-              fontSize="xs"
-              fontWeight="normal"
-              letterSpacing="wide"
-            >
-              Message
-            </FormLabel>
-            <Textarea
-              name="message"
-              value={formData.message}
-              onChange={_handleChange}
-            />
-          </FormControl>
-          <Button variant="primary" onClick={_handleSubmit}>
-            Send
-          </Button>
-        </Stack>
+            <Button variant="primary" disabled={isSubmitting} type="submit">
+              {isSubmitting ? <Spinner /> : 'Send'}
+            </Button>
+          </Stack>
+        </form>
       </Box>
     </Container>
   );
