fee0f3aea30e314b0cd96d38cbbced9d50a6474c
Chris Jardine <dev@keplux.com>
2023-01-26 23:26:37 -0500

    Update facebook share image url.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ const App = ({ Component, pageProps }: AppProps) => {
             siteName: 'Keplux Development',
             images: [
               {
-                url: 'https://i.imgur.com/5qsDLWr.png',
+                url: 'https://i.imgur.com/8lVjcsD.jpg',
               },
             ],
           }}
