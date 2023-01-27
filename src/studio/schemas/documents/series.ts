1d230061744625351e3594c2749f5051f6746226
Chris Jardine <dev@keplux.com>
2023-01-26 21:10:38 -0500

    Add description, cover photo, and seo fields to series schema.

--- src/studio/schemas/documents/series.ts
+++ src/studio/schemas/documents/series.ts
@@ -1,11 +1,15 @@
-import { defineType } from 'sanity';
 import { IoIosPaper } from '@react-icons/all-files/io/IoIosPaper';
+import { defineType } from 'sanity';
 
 const series = defineType({
   name: 'series',
   title: 'Series',
   icon: IoIosPaper,
   type: 'document',
+  groups: [
+    { name: 'info', title: 'Info', default: true },
+    { name: 'metadata', title: 'Metadata' },
+  ],
   fields: [
     {
       name: 'slug',
@@ -15,18 +19,41 @@ const series = defineType({
       options: {
         source: 'title',
       },
+      group: 'info',
     },
     {
       name: 'title',
       title: 'Title',
       type: 'string',
       validation: (Rule) => Rule.required().min(2),
+      group: 'info',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+      group: 'info',
+    },
+    {
+      name: 'coverPhoto',
+      title: 'Cover Photo',
+      type: 'coverPhoto',
+      group: 'info',
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'tags',
       title: 'Tags',
       type: 'array',
       of: [{ type: 'reference', to: [{ type: 'tag' }] }],
+      group: 'info',
+    },
+    {
+      name: 'seo',
+      title: 'SEO',
+      type: 'seo',
+      group: 'metadata',
     },
   ],
   preview: {
