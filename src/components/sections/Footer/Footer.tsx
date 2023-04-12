3eae14db930351e7baff60c198c92d259d589b71
Chris Jardine <dev@keplux.com>
2023-04-12 11:43:46 -0400

    Add featured by websites to footer.

--- src/components/sections/Footer/Footer.tsx
+++ src/components/sections/Footer/Footer.tsx
@@ -11,123 +11,191 @@ import {
 import { FaFacebook } from '@react-icons/all-files/fa/FaFacebook';
 import { FaLinkedinIn } from '@react-icons/all-files/fa/FaLinkedinIn';
 import Image from 'next/image';
+import designRush from '../../../../public/featured/design-rush-logo.png';
+import upcity from '../../../../public/featured/upcity-logo.png';
 import logo from '../../../../public/keplux-logo-full-light.png';
 import { KLink } from '../../core';
 
 const Footer = () => {
   return (
-    <Box
-      bg="whiteAlpha.50"
-      color="whiteAlpha.600"
-      shadow="2xl"
-      borderTopWidth={1}
-      borderColor="whiteAlpha.400"
-    >
-      <Container maxW="6xl" w="full" py={16}>
-        <Stack alignItems="center">
-          <SimpleGrid columns={{ base: 1, lg: 3 }} gap={8} w="full">
-            <Stack
-              alignItems={{ base: 'center', lg: 'flex-start' }}
-              spacing={8}
-            >
-              <Box position="relative" w="full" maxW={64}>
-                <Image
-                  src={logo}
-                  alt="Keplux Development logo."
-                  style={{ objectFit: 'contain' }}
-                />
-              </Box>
-              <Stack spacing={1} fontSize="sm" letterSpacing="wide">
-                <KLink href="mailto:contact@keplux.com">
-                  contact@keplux.com
-                </KLink>
-                <HStack>
-                  <KLink
-                    href="https://www.facebook.com/kepluxdev/"
-                    target="_blank"
-                  >
-                    <Icon
-                      as={FaFacebook}
-                      w={5}
-                      h={5}
-                      _hover={{ color: 'brand.500' }}
-                      transition="200ms ease-in-out"
-                      cursor="pointer"
-                      aria-label="View Keplux Development's Facebook page."
-                    />
-                  </KLink>
-                  <KLink
-                    href="https://www.linkedin.com/company/keplux-development/"
-                    target="_blank"
-                  >
-                    <Icon
-                      as={FaLinkedinIn}
-                      w={5}
-                      h={5}
-                      _hover={{ color: 'brand.500' }}
-                      transition="200ms ease-in-out"
-                      cursor="pointer"
-                      aria-label="View Keplux Development's LinkedIn page."
-                    />
-                  </KLink>
-                </HStack>
-              </Stack>
-              <Text mt={2} fontSize="xs" color="black0">
-                &#169; 2022 Keplux Development, LLC.
-              </Text>
-            </Stack>
-            <Stack>
-              <Box>
-                <Text
-                  fontSize="xl"
-                  textTransform="uppercase"
-                  fontWeight="semibold"
-                  color="whiteAlpha.800"
-                >
-                  Navigation
-                </Text>
-                <Divider w={16} borderWidth={2} borderColor="brand.600" />
-              </Box>
-              <Stack
-                spacing={2}
-                fontSize="sm"
-                textTransform="uppercase"
-                letterSpacing="wide"
-              >
-                <KLink href="/">Home</KLink>
-                <KLink href="/pricing">Pricing</KLink>
-                <KLink href="/our-work">Our Work</KLink>
-                <KLink href="/blog">Blog</KLink>
-                <KLink href="/contact">Contact</KLink>
-                <KLink href="/referral-program">Referral Program</KLink>
-                <KLink href="/refund-policy">Refund Policy</KLink>
-              </Stack>
-            </Stack>
-            <Stack>
-              <Box>
-                <Text
-                  fontSize="xl"
-                  textTransform="uppercase"
-                  fontWeight="semibold"
-                  color="whiteAlpha.800"
-                >
-                  About
-                </Text>
-                <Divider w={16} borderWidth={2} borderColor="brand.600" />
-              </Box>
-              <Stack spacing={1} fontSize="sm" letterSpacing="wide">
-                <Text>
-                  Keplux Development is a web design and development agency in
-                  Toledo, OH. We provide custom web design and development
-                  services, ecommerce websites, search engine optimization,
-                  website hosting and maintenance, and more.
-                </Text>
-              </Stack>
-            </Stack>
-          </SimpleGrid>
+    <>
+      <Box>
+        <Text
+          textTransform="uppercase"
+          fontSize="sm"
+          fontWeight="semibold"
+          color="whiteAlpha.600"
+          textAlign="center"
+        >
+          Featured on
+        </Text>
+        <Stack
+          mt={8}
+          mb={20}
+          gap={8}
+          direction={{ base: 'column', md: 'row' }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box
+            as="a"
+            href="https://upcity.com/profiles/keplux-development"
+            target="blank"
+            position="relative"
+            h={16}
+            maxW={64}
+            w="full"
+            filter="grayscale(75%)"
+            transition="200ms ease-in-out"
+            cursor="pointer"
+            _hover={{
+              filter: 'grayscale(0%)',
+            }}
+          >
+            <Image
+              src={upcity}
+              alt="Upcity logo"
+              fill
+              style={{ objectFit: 'contain' }}
+            />
+          </Box>
+          <Box
+            as="a"
+            href="https://www.designrush.com"
+            target="blank"
+            position="relative"
+            h={16}
+            maxW={64}
+            w="full"
+            filter="grayscale(75%)"
+            transition="200ms ease-in-out"
+            cursor="pointer"
+            _hover={{
+              filter: 'grayscale(0%)',
+            }}
+          >
+            <Image
+              src={designRush}
+              alt="Design Rush logo"
+              fill
+              style={{ objectFit: 'contain' }}
+            />
+          </Box>
         </Stack>
-      </Container>
-    </Box>
+      </Box>
+      <Box
+        bg="whiteAlpha.50"
+        color="whiteAlpha.600"
+        shadow="2xl"
+        borderTopWidth={1}
+        borderColor="whiteAlpha.400"
+      >
+        <Container maxW="6xl" w="full" py={16}>
+          <Stack alignItems="center">
+            <SimpleGrid columns={{ base: 1, lg: 3 }} gap={8} w="full">
+              <Stack
+                alignItems={{ base: 'center', lg: 'flex-start' }}
+                spacing={8}
+              >
+                <Box position="relative" w="full" maxW={64}>
+                  <Image
+                    src={logo}
+                    alt="Keplux Development logo."
+                    style={{ objectFit: 'contain' }}
+                  />
+                </Box>
+                <Stack spacing={1} fontSize="sm" letterSpacing="wide">
+                  <KLink href="mailto:contact@keplux.com">
+                    contact@keplux.com
+                  </KLink>
+                  <HStack>
+                    <KLink
+                      href="https://www.facebook.com/kepluxdev/"
+                      target="_blank"
+                    >
+                      <Icon
+                        as={FaFacebook}
+                        w={5}
+                        h={5}
+                        _hover={{ color: 'brand.500' }}
+                        transition="200ms ease-in-out"
+                        cursor="pointer"
+                        aria-label="View Keplux Development's Facebook page."
+                      />
+                    </KLink>
+                    <KLink
+                      href="https://www.linkedin.com/company/keplux-development/"
+                      target="_blank"
+                    >
+                      <Icon
+                        as={FaLinkedinIn}
+                        w={5}
+                        h={5}
+                        _hover={{ color: 'brand.500' }}
+                        transition="200ms ease-in-out"
+                        cursor="pointer"
+                        aria-label="View Keplux Development's LinkedIn page."
+                      />
+                    </KLink>
+                  </HStack>
+                </Stack>
+                <Text mt={2} fontSize="xs" color="black0">
+                  &#169; 2022 Keplux Development, LLC.
+                </Text>
+              </Stack>
+              <Stack>
+                <Box>
+                  <Text
+                    fontSize="xl"
+                    textTransform="uppercase"
+                    fontWeight="semibold"
+                    color="whiteAlpha.800"
+                  >
+                    Navigation
+                  </Text>
+                  <Divider w={16} borderWidth={2} borderColor="brand.600" />
+                </Box>
+                <Stack
+                  spacing={2}
+                  fontSize="sm"
+                  textTransform="uppercase"
+                  letterSpacing="wide"
+                >
+                  <KLink href="/">Home</KLink>
+                  <KLink href="/pricing">Pricing</KLink>
+                  <KLink href="/our-work">Our Work</KLink>
+                  <KLink href="/blog">Blog</KLink>
+                  <KLink href="/contact">Contact</KLink>
+                  <KLink href="/referral-program">Referral Program</KLink>
+                  <KLink href="/refund-policy">Refund Policy</KLink>
+                </Stack>
+              </Stack>
+              <Stack>
+                <Box>
+                  <Text
+                    fontSize="xl"
+                    textTransform="uppercase"
+                    fontWeight="semibold"
+                    color="whiteAlpha.800"
+                  >
+                    About
+                  </Text>
+                  <Divider w={16} borderWidth={2} borderColor="brand.600" />
+                </Box>
+                <Stack spacing={1} fontSize="sm" letterSpacing="wide">
+                  <Text>
+                    Keplux Development is a web design and development agency in
+                    Toledo, OH. We provide custom web design and development
+                    services, ecommerce websites, search engine optimization,
+                    website hosting and maintenance, and more.
+                  </Text>
+                </Stack>
+              </Stack>
+            </SimpleGrid>
+          </Stack>
+        </Container>
+      </Box>
+    </>
   );
 };
 
